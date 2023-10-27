8abe1f27e1c48a680633c282936b59e833288f19
mateicrainiceanu <100783062+mateicrainiceanu@users.noreply.github.com>
2023-10-27 11:51:47 +0300

    Fist commmit

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Muie7.java
@@ -0,0 +1,37 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+
+@TeleOp(name = "muie7 (Blocks to Java)")
+public class Muie7 extends LinearOpMode {
+
+    private DcMotor motor;
+    private DcMotor motor2;
+
+    private Servo
+
+
+    /**
+     * This function is executed when this Op Mode is selected from the Driver Station.
+     */
+    @Override
+    public void runOpMode() {
+        motor2 = hardwareMap.get(DcMotor.class, "motor2");
+        motor = hardwareMap.get(DcMotor.class, "motor");
+
+        // Put initialization blocks here.
+        waitForStart();
+        if (opModeIsActive()) {
+            // Put run blocks here.
+            while (opModeIsActive()) {
+                // Put loop blocks here.
+                telemetry.update();
+                motor.setPower(1);
+                motor2.setPower(-1);
+            }
+        }
+    }
+}
